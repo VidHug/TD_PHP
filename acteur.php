a306451890a890541f23826446e2ad1b01cdce76
hugovidal <hvidal@viaxoft.com>
2017-11-13 10:05:38 +0100

    ajout du css mais retrait des couleurs

--- acteur.php
+++ acteur.php
@@ -5,7 +5,7 @@
   <head>
     <meta charset="utf-8">
   	<title><?php echo getFilm()['titre'] ?></title>
-  	<!-- <link rel="stylesheet" href="watchmen.css"> -->
+  	<link rel="stylesheet" href="watchmen.css">
   	<link rel="icon" href="image/logo.png" />
   </head>
 
