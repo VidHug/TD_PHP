34c606b12ca2804fb9dfbc764e51c81ddd6c5cc5
hugovidal <hvidal@viaxoft.com>
2017-12-05 12:15:22 +0100

    F.A.Q tri selon les cliques

--- contenu_film.php
+++ contenu_film.php
@@ -63,105 +63,126 @@
 
         <h2>La foire aux questions !</h2>
         <dl id="faq">
-            <dt>Question 1</dt>
-            <dd>
-                <dl>
-                    <dt>réponse 1 partie 1</dt>
-                    <dd>détail 1</dd>
-                    <dt>réponse 1 partie 2</dt>
-                    <dd>détail 2</dd>
-                </dl>
-            </dd>
+            <dl>
+                <dt>Question 1</dt>
+                <dd>
+                    <dl>
+                        <dt>réponse 1 partie 1</dt>
+                        <dd>détail 1</dd>
+                        <dt>réponse 1 partie 2</dt>
+                        <dd>détail 2</dd>
+                    </dl>
+                </dd>
+            </dl>
 
-            <dt>Question 2</dt>
-            <dd>
-                <dl>
-                    <dt>réponse 1 partie 1</dt>
-                    <dd>détail 1</dd>
-                    <dt>réponse 1 partie 2</dt>
-                    <dd>détail 2</dd>
-                </dl>
-            </dd>
+            <dl>
+                <dt>Question 2</dt>
+                <dd>
+                    <dl>
+                        <dt>réponse 1 partie 1</dt>
+                        <dd>détail 1</dd>
+                        <dt>réponse 1 partie 2</dt>
+                        <dd>détail 2</dd>
+                    </dl>
+                </dd>
+            </dl>
 
-            <dt>Question 3</dt>
-            <dd>
-                <dl>
-                    <dt>réponse 1 partie 1</dt>
-                    <dd>détail 1</dd>
-                    <dt>réponse 1 partie 2</dt>
-                    <dd>détail 2</dd>
-                </dl>
-            </dd>
+            <dl>
+                <dt>Question 3</dt>
+                <dd>
+                    <dl>
+                        <dt>réponse 1 partie 1</dt>
+                        <dd>détail 1</dd>
+                        <dt>réponse 1 partie 2</dt>
+                        <dd>détail 2</dd>
+                    </dl>
+                </dd>
+            </dl>
 
-            <dt>Question 4</dt>
-            <dd>
-                <dl>
-                    <dt>réponse 1 partie 1</dt>
-                    <dd>détail 1</dd>
-                    <dt>réponse 1 partie 2</dt>
-                    <dd>détail 2</dd>
-                </dl>
-            </dd>
+            <dl>
+                <dt>Question 4</dt>
+                <dd>
+                    <dl>
+                        <dt>réponse 1 partie 1</dt>
+                        <dd>détail 1</dd>
+                        <dt>réponse 1 partie 2</dt>
+                        <dd>détail 2</dd>
+                    </dl>
+                </dd>
+            </dl>
 
-            <dt>Question 5</dt>
-            <dd>
-                <dl>
-                    <dt>réponse 1 partie 1</dt>
-                    <dd>détail 1</dd>
-                    <dt>réponse 1 partie 2</dt>
-                    <dd>détail 2</dd>
-                </dl>
-            </dd>
+            <dl>
+                <dt>Question 5</dt>
+                <dd>
+                    <dl>
+                        <dt>réponse 1 partie 1</dt>
+                        <dd>détail 1</dd>
+                        <dt>réponse 1 partie 2</dt>
+                        <dd>détail 2</dd>
+                    </dl>
+                </dd>
+            </dl>
 
-            <dt>Question 6</dt>
-            <dd>
-                <dl>
-                    <dt>réponse 1 partie 1</dt>
-                    <dd>détail 1</dd>
-                    <dt>réponse 1 partie 2</dt>
-                    <dd>détail 2</dd>
-                </dl>
-            </dd>
+            <dl>
+                <dt>Question 6</dt>
+                <dd>
+                    <dl>
+                        <dt>réponse 1 partie 1</dt>
+                        <dd>détail 1</dd>
+                        <dt>réponse 1 partie 2</dt>
+                        <dd>détail 2</dd>
+                    </dl>
+                </dd>
+            </dl>
 
-            <dt>Question 7</dt>
-            <dd>
-                <dl>
-                    <dt>réponse 1 partie 1</dt>
-                    <dd>détail 1</dd>
-                    <dt>réponse 1 partie 2</dt>
-                    <dd>détail 2</dd>
-                </dl>
-            </dd>
+            <dl>
+                <dt>Question 7</dt>
+                <dd>
+                    <dl>
+                        <dt>réponse 1 partie 1</dt>
+                        <dd>détail 1</dd>
+                        <dt>réponse 1 partie 2</dt>
+                        <dd>détail 2</dd>
+                    </dl>
+                </dd>
+            </dl>
 
-            <dt>Question 8</dt>
-            <dd>
-                <dl>
-                    <dt>réponse 1 partie 1</dt>
-                    <dd>détail 1</dd>
-                    <dt>réponse 1 partie 2</dt>
-                    <dd>détail 2</dd>
-                </dl>
-            </dd>
+            <dl>
+                <dt>Question 8</dt>
+                <dd>
+                    <dl>
+                        <dt>réponse 1 partie 1</dt>
+                        <dd>détail 1</dd>
+                        <dt>réponse 1 partie 2</dt>
+                        <dd>détail 2</dd>
+                    </dl>
+                </dd>
+            </dl>
 
-            <dt>Question 9</dt>
-            <dd>
-                <dl>
-                    <dt>réponse 1 partie 1</dt>
-                    <dd>détail 1</dd>
-                    <dt>réponse 1 partie 2</dt>
-                    <dd>détail 2</dd>
-                </dl>
-            </dd>
+            <dl>
+                <dt>Question 9</dt>
+                <dd>
+                    <dl>
+                        <dt>réponse 1 partie 1</dt>
+                        <dd>détail 1</dd>
+                        <dt>réponse 1 partie 2</dt>
+                        <dd>détail 2</dd>
+                    </dl>
+                </dd>
+            </dl>
 
-            <dt>Question 10</dt>
-            <dd>
-                <dl>
-                    <dt>réponse 1 partie 1</dt>
-                    <dd>détail 1</dd>
-                    <dt>réponse 1 partie 2</dt>
-                    <dd>détail 2</dd>
-                </dl>
-            </dd>
+            <dl>
+                <dt>Question 10</dt>
+                <dd>
+                    <dl>
+                        <dt>réponse 1 partie 1</dt>
+                        <dd>détail 1</dd>
+                        <dt>réponse 1 partie 2</dt>
+                        <dd>détail 2</dd>
+                    </dl>
+                </dd>
+            </dl>
+
         </dl>
 	</section>
 
--- script_film.js
+++ script_film.js
@@ -17,15 +17,24 @@ $(document).ready(function(){
         $('header a').toggle(500);
     });
 
-    $('#faq > dt').each(function () {
+    $('#faq > dl > dt').each(function () {
         $(this).data('text',$(this).html());
         $(this).data('nb',0).append(' : ' + $(this).data('nb') + ' clique(s)');
+        $(this).data('open',false);
     }).click(function () {
-        if ($(this).data('nb') == 0) {
+        if (!$(this).data('open')) {
             incrementeData($(this), 'nb', 1);
             $(this).html($(this).data('text') + ' : ' + $(this).data('nb') + ' clique(s)');
+            $('#faq > dl > dt').data('open',false);
+            $(this).data('open',true);
         }
-        $('#faq > dd').css('color', 'white');
+        $('#faq > dl > dd').css('color', 'white');
         $(this).next().css('color', 'black');
+    });
+
+    $('#faq > dl').click(function () {
+        while($(this).children().data('nb') >= $(this).prev().children().data('nb')){
+            $(this).insertBefore($(this).prev());
+        }
     });
 });
